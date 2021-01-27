6a67d8c1b21a349a4853d80a6424ac2184601883
Roddy Vitali <roddicks@gmail.com>
2021-01-26 21:09:38 -0300

    add prints test

--- src/test/java/selenium/AppTest.java
+++ src/test/java/selenium/AppTest.java
@@ -31,24 +31,30 @@ public class AppTest {
 
     @Test
     public void shouldAnswerWithTrue() {
-        System.out.println("Iniciando Pruebas...");
+        System.out.println("Iniciando Pruebas Google...");
+        System.out.println("Go To Google...");
         WebElement searchbox = driver.findElement(By.name("q"));
         searchbox.sendKeys("HandBook Devops");
         searchbox.submit();
+        System.out.println("Search HandBook Devops...");
         assertEquals("HandBook Devops", driver.getTitle().split("-", 0)[0].trim());
         driver.findElement(By.linkText("Imágenes")).click();
+        System.out.println("HandBook Devops Imágenes...");
         assertEquals("HandBook Devops - Google Search", driver.getTitle().trim());
         driver.findElement(By.linkText("Shopping")).click();
+        System.out.println("HandBook Devops Shopping...");
         assertEquals("HandBook Devops - Google Shopping", driver.getTitle().trim());
     }
 
     @Test
     public void shouldSearchInAmazon() {
-        System.out.println("Amazon Pruebas...");
+        System.out.println("Iniciando Pruebas Amazon...");
+        System.out.println("Go To Amazon...");
         driver.get("https://www.amazon.com");
         WebElement searchbox = driver.findElement(By.id("twotabsearchtextbox"));
         searchbox.sendKeys("The Phoenix Project");
         searchbox.submit();
+        System.out.println("Search The Phoenix Project...");
         assertEquals("The Phoenix Project", driver.getTitle().split(":", 0)[1].trim());
     }
 }
